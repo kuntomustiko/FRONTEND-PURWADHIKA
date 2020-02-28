a081971a7ebc8c913b762a3e9d0320ef3dd72a71
Kunto Mustiko <kuntomustiko@gmail.com>
2020-02-28 09:12:11 +0700

    mulai hari ke-3 28-02-2020 front end purwadhika

---
+++ soalhacckerrank.js
@@ -0,0 +1,132 @@
+// function diagonalDifference(arr) {
+//     var kirkanan = 0
+//     var kanankir = 0
+
+//     var lengthArr = arr.length
+//     var sum = 0
+
+//     for (var b = 0; b < arr.length; b++) {
+//         kirkanan += arr[b][b]
+//         kanankir += arr[b][lengthArr - 1 - b]
+//     }
+
+//     sum = kirkanan - kanankir
+//     return sum
+
+// }
+
+// var arr = [
+//     [11, 2, 4],
+//     [4, 5, 6],
+//     [10, 8, -12]
+// ]
+
+// console.log(diagonalDifference(arr));
+
+//-------------------------------------
+
+// Complete the plusMinus function below.
+// function plusMinus(arr) {
+
+//     var plus = 0
+//     var minus = 0
+//     var zero = 0
+//     var arrlenght = arr.length
+
+//     for (let index = 0; index < arr.length; index++) {
+//         if (arr[index] > 0) {
+//             plus++
+//         } else if (arr[index] < 0) {
+//             minus++
+//         } else if (arr[index] == 0) {
+//             zero++
+//         }
+//     }
+
+//     var divPlus = plus / arrlenght
+//     var divMinus = minus / arrlenght
+//     var divZero = zero / arrlenght
+
+//     console.log(divPlus.toFixed(arrlenght));
+//     console.log(divMinus.toFixed(arrlenght));
+//     console.log(divZero.toFixed(arrlenght));
+// }
+
+// var arr = [-4, 3, -9, 0, 4, 1, ]
+
+// plusMinus(arr)
+
+//----------------------------------------
+
+// Complete the staircase function below.
+// function staircase(n) {
+
+//     var z = ''
+//     var strip = 0
+//     var i = 0
+//     for (i = 0; i < n; i++) {
+//         for (strip = 1; strip < n - i; strip++) {
+//             z += '-'
+//         }
+//         for (let pagar = 0; pagar <= i; pagar++) {
+//             z += '#'
+//         }
+//         z += '\n'
+//     }
+
+//     console.log(z);
+
+// }
+
+// staircase(6)
+
+//--------------------------
+// function miniMaxSum(arr) {
+
+//     var min = 0
+//     var max = 0
+//     var minmin = Math.min.apply(null, arr)
+//     var maxmax = Math.max.apply(null, arr)
+//     if (minmin != maxmax) {
+//         for (let index = 0; index < arr.length; index++) {
+//             if (arr[index] != minmin) {
+//                 max += arr[index]
+//             }
+//             if (arr[index] != maxmax) {
+//                 min += arr[index]
+//             }
+//         }
+//     } else {
+
+//         for (let index = 1; index < arr.length; index++) {
+//             max += arr[index]
+//             min += arr[index]
+//         }
+//     }
+//     console.log(`${min} ${max}`);
+// }
+
+// var arr = [5, 5, 5, 5, 5]
+
+// miniMaxSum(arr)
+
+// Complete the birthdayCakeCandles function below.
+function birthdayCakeCandles(ar) {
+
+    var sumSame = 0
+    var maxmax = Math.max.apply(null, ar)
+
+    for (let index = 0; index < ar.length; index++) {
+        if (ar[index] == maxmax) {
+            sumSame++
+        }
+    }
+    console.log(typeof (sumSame));
+    console.log(sumSame);
+
+}
+
+// var ar = [3, 2, 1, 3, 2, 2]
+var ar = [3, 2, 1, 3]
+
+birthdayCakeCandles(ar)
