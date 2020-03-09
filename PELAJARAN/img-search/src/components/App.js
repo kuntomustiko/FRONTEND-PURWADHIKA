193477bddb1b01602c3a274b1c3dc8f7d23ce516
Kunto Mustiko <kuntomustiko@gmail.com>
2020-03-09 10:03:01 +0700

    coding pertama di react

---
+++ PELAJARAN/img-search/src/components/App.js
@@ -0,0 +1,39 @@
+// ROOT COMPONENTS
+import React from 'react' // utama di tulis
+
+// membuat komponen berbasis class
+class App extends React.Component { // utama di tulis
+
+    // Function yang wajib ada, apa yang ingin kita tampilkan, akan kita tulis di sini
+    // Funtion render hanya boleh me-return satu buah tag html
+    render() {
+        return (
+            // html di sini
+            <div>
+                <h1> REACT 101 </h1>
+            </div>
+        )
+    }
+}
+
+// Karena akan di import di file lain
+export default App // utama di tulis
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// React Hooks
---
+++ PELAJARAN/img-search/src/index.js
@@ -0,0 +1,9 @@
+// React, untuk memproses kode menjadi sebuah komponen
+import React from 'react'
+// Digunakan untuk memanipulasi komponen / menghubungkan App.js ke Index.html
+import ReactDOM from 'react-dom'
+// Extension .js  tidak wajib di tulis
+import App from './components/App'
+
+// ReactDOM.render(komponen, target)
+ReactDOM.render(<App />, document.getElementById('root'))
