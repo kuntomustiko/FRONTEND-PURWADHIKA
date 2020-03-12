7fe2c342fa124e246b8256145079674fe155baac
Kunto Mustiko <kuntomustiko@gmail.com>
2020-03-12 10:29:38 +0700

    buat simpelmerce react

---
+++ PELAJARAN/simplemerce/src/components/App.js
@@ -0,0 +1,21 @@
+import React, {
+    Component
+} from 'react'
+
+// Components
+import Header from './Header'
+import Home from './Home'
+import Register from './Register'
+import Login from './Login'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default App
