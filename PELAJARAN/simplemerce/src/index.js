402f72c8a7f2595e91e2c63cee43043ce7e4f0ee
Kunto Mustiko <kuntomustiko@gmail.com>
2020-03-24 10:25:19 +0700

    redux devtools

--- PELAJARAN/simplemerce/src/index.js
+++ PELAJARAN/simplemerce/src/index.js
@@ -1,5 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './components/App'
+import reducers from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+
+// kadang orang bilang store kadang orang bilang state
+let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+// konfigurasi redux
--- PELAJARAN/simplemerce/src/reducers/index.js
+++ PELAJARAN/simplemerce/src/reducers/index.js
@@ -2,7 +2,7 @@
 import { combineReducers } from 'redux'
 import AuthReducer from './AuthReducer'
 
-combineReducers(
+export default combineReducers(
     {
         auth: AuthReducer
     }
